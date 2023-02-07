ccaf64f956dc05d166f293e429a59defe0881054
guangtuan <zhuangguangtuan@gmail.com>
2023-02-07 20:52:05 +0800

    refactor: httpServer.ts

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,35 @@
-import bodyParser from 'body-parser'
 import express from 'express'
 import * as env from './framework/env'
 import * as db from './framework/db'
 import urlJoin from './url'
+import httpServer from './framework/httpServer'
 
 import spaceApi from './space/api'
 import spaceDetailApi from './resources/api'
 
 import startMediaServer from './media/startServer'
 
-const createApp = (): express.Express => {
-    const app = express()
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({ extended: true }))
-    return app
-}
+const apiList = [spaceApi, spaceDetailApi]
 
 const appInit = async () => {
-    const app = createApp()
+    const app = httpServer.createApp()
 
-    ;[spaceApi, spaceDetailApi].forEach(
-        async ({ base, apis }): Promise<void> => {
-            apis.forEach(async (api) => {
-                const actualUrl = urlJoin(['api', base, api.url])
-                console.log(
-                    `actual url is ${actualUrl}, method is ${api.method}`,
-                )
-                app[api.method](
-                    '/' + actualUrl,
-                    async (req: express.Request, res: express.Response) => {
-                        console.log(`api ${JSON.stringify(req.body)}`)
-                        const result = await api.handler(req, res)
-                        res.status(200)
-                        res.json(result)
-                        res.end()
-                    },
-                )
-            })
-        },
-    )
+    apiList.forEach(async ({ base, apis }): Promise<void> => {
+        apis.forEach(async (api) => {
+            const actualUrl = urlJoin(['api', base, api.url])
+            console.log(`actual url is ${actualUrl}, method is ${api.method}`)
+            app[api.method](
+                '/' + actualUrl,
+                async (req: express.Request, res: express.Response) => {
+                    console.log(`api ${JSON.stringify(req.body)}`)
+                    const result = await api.handler(req, res)
+                    res.status(200)
+                    res.json(result)
+                    res.end()
+                },
+            )
+        })
+    })
 
     app.listen(env.webPort, () => {
         console.log('app listen on', env.webPort)
